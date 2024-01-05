dbcd55b503e1add0f8a8ee37e8c995cd493aebc1
Said Babaiev <babaiev.info@gmail.com>
2024-01-05 19:56:12 +0100

    made sidebar menu

---
+++ components/layout/Menu.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { MENU_DATA } from "./menu.data"
+</script>
+
+<template>
+	<div>
+		<NuxtLink
+			class="flex items-center py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover:shadow transition-all mb-2.5"
+			v-for="menu in MENU_DATA"
+			:key="menu.name"
+			:to="menu.url"
+		>
+			<Icon :name="menu.icon" class="mr-3" size="18px" />
+			<span>{{ menu.name }}</span>
+		</NuxtLink>
+	</div>
+</template>
--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -5,12 +5,14 @@ const value = 20
 <template>
 	<aside class="px-5 py-8 bg-sidebar h-full relative w-full">
 		<!-- TODO: ds  -->
-		<NuxtLink to="/" class="mb-10 block">
-			<NuxtImg src="/logo.svg" width="100px" class="mx-auto" />
+		<NuxtLink to="/" class="mb-7 block">
+			<NuxtImg src="/logo.svg" width="120px" class="mx-auto" />
 		</NuxtLink>
 
 		<button class="absolute top-2 right-3 transition-colors hover:text-primary">
 			<Icon name="line-md:logout" size="20px" />
 		</button>
+
+		<LayoutMenu />
 	</aside>
 </template>
