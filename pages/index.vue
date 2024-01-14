c4c4f19866fcdabf163675a3fcb6481212f91f4d
Said Babaiev <babaiev.info@gmail.com>
2024-01-14 19:09:19 +0100

    made generate gradient kanban column titles

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { generateColumnStyle } from "@/components/kanban/generate-gradient"
 import type { ICard, IColumn } from "@/components/kanban/kanban.types"
 import { useMutation } from "@tanstack/vue-query"
 import dayjs from "dayjs"
@@ -66,7 +67,10 @@ function handleDrop(targetColumn: IColumn) {
 						@dragover="handleDragOver"
 						@drop="() => handleDrop(column)"
 					>
-						<div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+						<div
+							class="rounded bg-slate-700 py-1 px-5 mb-2 text-center"
+							:style="generateColumnStyle(index, data?.length)"
+						>
 							{{ column.name }}
 						</div>
 
